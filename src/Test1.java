585100f0b3d0f1a07cae01e22dd4a319efa581a4
mohsi <mohsi@LAPTOP-V9LR9B12.home>
2021-06-15 16:51:55 -0400

    first Commit

---
+++ src/Test1.java
@@ -0,0 +1,27 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	String hungry = "yes";
+	if(hungry=="yes") {
+		System.out.println("Eat something.......");
+		
+	}else {
+		System.out.println("Do work......");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
